9628da1526bcf0147cf919e1e54f64121ef524b7
OleksandrBrukhalskyi <bruhalskyi.o@gmail.com>
2023-03-26 11:02:46 +0300

    feat: implement email sending with final score.

--- app/src/main/java/com/example/dummyquizapp/ResultActivity.java
+++ app/src/main/java/com/example/dummyquizapp/ResultActivity.java
@@ -4,7 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class ResultActivity extends AppCompatActivity {
     TextView resultView;
@@ -14,16 +27,55 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         resultView = findViewById(R.id.textView);
-
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             String name = bundle.getString("name");
+            String email = bundle.getString("email");
             int score = bundle.getInt("score");
             int amountOfQuestions = bundle.getInt("amountOfQuestions");
-            System.out.println("NAME: " + name);
-            System.out.println("SCORE: " + score);
-            System.out.println("QUESTS: " + amountOfQuestions);
-            resultView.setText( name + "!" + " Ваш результат: " + score + " з " + amountOfQuestions);
+
+            String senderEmail = "senderEmail@gmail.com";
+            String senderPassword = "senderPassword";
+
+            String host = "smtp.gmail.com";
+
+            Properties properties = System.getProperties();
+            properties.put("mail.smtp.host", host);
+            properties.put("mail.smtp.socketFactory.port", "465");
+            properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.debug", "true");
+            properties.put("mail.smtp.port", "465");
+
+
+            Session session = Session.getInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(senderEmail, senderPassword);
+                }
+            });
+
+            MimeMessage mimeMessage = new MimeMessage(session);
+            try {
+                mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+                mimeMessage.setSubject("Subject: AndroidQuizApp");
+                mimeMessage.setText(name + "!" + "\n Ваш результат: " + score + " з " + amountOfQuestions);
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Transport.send(mimeMessage);
+                    } catch (MessagingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+
+            resultView.setText(name + "!" + " Ваш результат: " + score + " з " + amountOfQuestions);
         }
 
 
